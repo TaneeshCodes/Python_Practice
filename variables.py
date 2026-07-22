db0d2d29ee97c7198dfd5c4d6da4890f91619710
TaneeshCodes <taneeshsawant05@gmail.com>
2026-07-22 07:19:37 +0000

    variables done

--- variables.py
+++ variables.py
@@ -7,3 +7,30 @@ clg = "Atharva College of Engineering"
 print(f"Hello {first_name} welcome to {clg}")
 
 print(first_name)
+
+#Integers
+
+age = int(input("Enter your age: "))
+
+if age > 18:
+    print("You are allowed to vote")
+else:
+    print("You are not allowed to vote")
+
+print(f"hello my age is {age}")
+print(age)
+
+#Float
+
+price = 10.99
+print(f"your total bill is around {price} dollars")
+
+#Boolean
+
+is_student = int(input("Are you a student?"))
+
+if is_student == 1:
+    print("You are a student")
+else:
+    print("You are not a student")
+
